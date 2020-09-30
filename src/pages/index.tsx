f14ac3862961f49946370dfbffeb6a2345e2358e
TheOllieJT <contact@olliejt.com>
2020-09-30 20:17:47 +0200

    Adds example component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import { ExampleComponent } from "../components/example";
 
 const Home = (): React.ReactElement => {
 	return (
@@ -9,10 +10,7 @@ const Home = (): React.ReactElement => {
 			</Head>
 
 			<main>
-				<div className="p-4 shadow rounded bg-white">
-					<h1 className="text-purple-500 leading-normal">Next.js</h1>
-					<p className="text-gray-500">with Tailwind CSS</p>
-				</div>
+				<ExampleComponent />
 			</main>
 		</div>
 	);
