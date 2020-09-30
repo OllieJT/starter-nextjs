f01d7ba916c42d71dca75f9e760aa2795800fd92
TheOllieJT <contact@olliejt.com>
2020-09-30 18:23:57 +0200

    Restructures Project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+import * as React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render(): React.ReactElement {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<Main />
+
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,16 +33,14 @@ const Home = () => {
 
 					<a
 						className={styles.card}
-						href="https://github.com/vercel/next.js/tree/master/examples"
-					>
+						href="https://github.com/vercel/next.js/tree/master/examples">
 						<h3>Examples &rarr;</h3>
 						<p>Discover and deploy boilerplate example Next.js projects.</p>
 					</a>
 
 					<a
 						className={styles.card}
-						href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					>
+						href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
 						<h3>Deploy &rarr;</h3>
 						<p>
 							Instantly deploy your Next.js site to a public URL with
@@ -56,8 +54,7 @@ const Home = () => {
 				<a
 					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
 					rel="noopener noreferrer"
-					target="_blank"
-				>
+					target="_blank">
 					Powered by{" "}
 					<img alt="Vercel Logo" className={styles.logo} src="/vercel.svg" />
 				</a>
