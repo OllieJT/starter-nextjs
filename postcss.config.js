14348f506e506ad3f5070fe072e0e8ed08b86b43
TheOllieJT <contact@olliejt.com>
2020-09-30 19:00:31 +0200

    Adds PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+const isProd = process.env.NODE_ENV === "production";
+
+const prodPlugins = isProd
+	? [
+			"autoprefixer",
+			"postcss-flexbugs-fixes",
+			[
+				"postcss-preset-env",
+				{
+					autoprefixer: {
+						flexbox: "no-2009"
+					},
+					stage: 3,
+					features: {
+						"custom-properties": false
+					}
+				}
+			]
+	  ]
+	: [];
+
+module.exports = {
+	plugins: ["postcss-nested", ...prodPlugins]
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.scss";
+import "../styles/globals.css";
 import Head from "next/head";
 import type { AppProps /* , AppContext */ } from "next/app";
 
