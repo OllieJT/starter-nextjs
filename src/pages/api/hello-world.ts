47935d4e087ecbd5715b046e1f20add614b29d3a
TheOllieJT <contact@olliejt.com>
2020-10-10 02:29:27 +0200

    Demo API

---
+++ src/pages/api/hello-world.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { NextApiRequest, NextApiResponse } from "next";
+
+export const HelloWorld = async (
+	req: NextApiRequest,
+	res: NextApiResponse<any>
+): Promise<void> => {
+	const { name } = req.query;
+
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "application/json");
+	res.end(JSON.stringify({ msg: `Hello ${name || "world"}` }));
+};
+
+export default HelloWorld;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import Head from "next/head";
+import { GetServerSideProps } from "next";
 import { ExampleComponent } from "../components/example";
 
-const Home = (): React.ReactElement => {
+export const getServerSideProps: GetServerSideProps<any> = async () => {
+	// Fetch data from external API
+	const res = await fetch(`http://localhost:3000/api/hello-world?name=Ollie`);
+	const data = await res.json();
+
+	// Pass data to the page via props
+	return { props: { data } };
+};
+
+const Home = ({ data }): React.ReactElement => {
 	return (
 		<div>
 			<Head>
@@ -10,6 +20,7 @@ const Home = (): React.ReactElement => {
 			</Head>
 
 			<main>
+				<pre>{JSON.stringify(data, null, 4)}</pre>
 				<ExampleComponent />
 			</main>
 		</div>
