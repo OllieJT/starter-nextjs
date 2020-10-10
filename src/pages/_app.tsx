aaefa1ddb9c3edbcb6b21e54642ddafc36c7373c
TheOllieJT <contact@olliejt.com>
2020-10-10 13:13:43 +0200

    Fixes styling setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../../styles/index.css";
+import "../styles/index.css";
 import React from "react";
 import Head from "next/head";
 import type { AppProps /* , AppContext */ } from "next/app";
