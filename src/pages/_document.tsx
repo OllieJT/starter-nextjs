6a02a2ac95cd0b23cb66136b8fc0cfe2f9052a2b
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2021-06-02 13:43:51 +0100

    Simplify getInitialProps

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx: any) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
+		return await Document.getInitialProps(ctx);
 	}
 
 	render(): React.ReactElement {
