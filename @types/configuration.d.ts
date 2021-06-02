4d2ba4b252ab71016720b614fdfae736d25c394d
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2021-06-02 13:46:09 +0100

    MetadataConfiguration Cleanup

---
+++ @types/configuration.d.ts
@@ -0,0 +1,9 @@
+interface MetadataConfiguration {
+	color: string;
+	site_name: string;
+	site_url: string;
+	site_title: string;
+
+	twitter_username?: string;
+	twitter_id: string;
+}
--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,16 +1,6 @@
-interface Configuration {
-	color: string;
-	site_name: string;
-	site_url: string;
-	site_title: string;
-
-	twitter_username?: string;
-	twitter_id: string;
-}
-
 const isDev = process.env.NODE_ENV === "development";
 
-export const configuration: Configuration = {
+export const configuration: MetadataConfiguration = {
 	color: "FFC92A",
 
 	site_name: "SiteName",
